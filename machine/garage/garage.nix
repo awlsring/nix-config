fc4921d40f75acea573d0529ff53247adeb91a2e
Matthew Rawlings <contact@matthewrawlings.com>
2024-05-01 22:05:35 -0700

    feat: garage secrets

--- machine/garage/garage.nix
+++ machine/garage/garage.nix
@@ -1,11 +1,39 @@
 { config, inputs, pkgs, ... }:
 let
   unstable = inputs.unstable.legacyPackages.x86_64-linux;
+  garageUser = "garage";
 in
 {
     environment.systemPackages = with unstable; [
         garage
     ];
+
+    sops.secrets = {
+        "garage/rpc" = {
+            owner = garageUser;
+        };
+        "garage/admin" = {
+            owner = garageUser;
+        };
+        "garage/metrics" = {
+            owner = garageUser;
+        };
+    };
+
+    users = {
+        users = {
+            ${garageUser} = {
+                isSystemUSer = true;
+                group = garageUser;
+            };
+        };
+        groups = {
+            ${garageUser} = { };          
+        };
+    };
+
+    systemd.services.garage.serviceConfig.User = garageUser;
+    systemd.services.garage.serviceConfig.Group = garageUser;
 
     networking.firewall.allowedTCPPorts = [ 3900 3901 3902 ];
 
@@ -15,6 +43,9 @@ in
         settings = {
             db_engine = "lmdb";
             replication_mode = "3";
+
+            metadata_dir = "/var/lib/garage/meta";
+            data_dir = "/var/lib/garage/data";
 
             rpc_bind_addr = "[::]:3901";
             rpc_secret_file = config.sops.secrets."garage/rpc".path;
@@ -26,7 +57,16 @@ in
 
             s3_web = {
                 bind_addr = "[::]:3902";
-                root_domain = "drigs.org";
+            };
+
+            k2v_api = {
+                api_bind_addr = "[::]:3904";
+            };
+
+            admin = {
+                api_bind_addr = "[::]:3903";
+                admin_token_file = config.sops.secrets."garage/admin".path;
+                metrics_token_file = config.sops.secrets."garage/metrics".path;
             };
         };
     };
