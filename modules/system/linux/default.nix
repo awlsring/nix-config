e1739f8c0d2c2f2c5da12de48d7169681b6f6a48
awlsring <contact@matthewrawlings.com>
2024-05-27 14:35:19 -0700

    feat(linux): passwordless sudo

--- modules/system/linux/default.nix
+++ modules/system/linux/default.nix
@@ -6,6 +6,18 @@
     inputs.sops-nix.nixosModules.sops
   ];
 
+  security.sudo.extraRules = [
+    {
+      users = ["privileged_user"];
+      commands = [
+        {
+          command = "ALL";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+
   # System Fonts
   fonts.fontconfig = {
     defaultFonts = {
