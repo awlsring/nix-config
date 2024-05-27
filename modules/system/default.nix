85605eb8129ae78fc08146f56ffa37aba4135643
Matthew Rawlings <contact@matthewrawlings.com>
2024-05-26 23:22:29 -0700

    fix(system): fontconfig nixos only

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -51,13 +51,6 @@
       atkinson-hyperlegible
       jetbrains-mono
     ];
-    fontconfig = {
-      defaultFonts = {
-        monospace = ["JetBrainsMono Nerd Font Mono"];
-        sansSerif = ["JetBrainsMono Nerd Font Mono"];
-        serif = ["JetBrainsMono Nerd Font Mono"];
-      };
-    };
   };
 
   programs = {
--- modules/system/linux/default.nix
+++ modules/system/linux/default.nix
@@ -5,4 +5,12 @@
     inputs.stylix.nixosModules.stylix
     inputs.sops-nix.nixosModules.sops
   ];
+
+  fontconfig = {
+    defaultFonts = {
+      monospace = ["JetBrainsMono Nerd Font Mono"];
+      sansSerif = ["JetBrainsMono Nerd Font Mono"];
+      serif = ["JetBrainsMono Nerd Font Mono"];
+    };
+  };
 }
