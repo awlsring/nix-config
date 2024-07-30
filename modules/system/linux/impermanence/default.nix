151138e1cccfa7523c6598a0c9e4003c921a994d
Matthew Rawlings <contact@matthewrawlings.com>
2024-07-29 23:03:09 -0700

    fix(impermanence): sops config, list append

--- modules/system/linux/impermanence/default.nix
+++ modules/system/linux/impermanence/default.nix
@@ -51,37 +51,31 @@
     fileSystems."/persist".neededForBoot = true;
     environment.persistence."/persist/system" = {
       hideMounts = true;
-      directories = [
-        "/etc/nixos"
-        "/var/log"
-        "/var/lib/bluetooth"
-        "/var/lib/nixos"
-        "/var/lib/systemd/coredump"
-        "/etc/NetworkManager/system-connections"
-        {
-          directory = "/var/lib/colord";
-          user = "colord";
-          group = "colord";
-          mode = "u=rwx,g=rx,o=";
-        }
-        (
-          if config.impermanence.persistedDirectories != []
-          then lib.mkIf config.impermanence.persistedDirectories
-          else []
-        )
-      ];
-      files = [
-        "/etc/machine-id"
-        {
-          file = "/var/keys/secret_file";
-          parentDirectory = {mode = "u=rwx,g=,o=";};
-        }
-        (
-          if config.impermanence.persistedFiles != []
-          then lib.mkIf config.impermanence.persistedFiles
-          else []
-        )
-      ];
+      directories =
+        [
+          "/etc/nixos"
+          "/var/log"
+          "/var/lib/bluetooth"
+          "/var/lib/nixos"
+          "/var/lib/systemd/coredump"
+          "/etc/NetworkManager/system-connections"
+          {
+            directory = "/var/lib/colord";
+            user = "colord";
+            group = "colord";
+            mode = "u=rwx,g=rx,o=";
+          }
+        ]
+        ++ config.impermanence.persistedDirectories;
+      files =
+        [
+          "/etc/machine-id"
+          {
+            file = "/var/keys/secret_file";
+            parentDirectory = {mode = "u=rwx,g=,o=";};
+          }
+        ]
+        ++ config.impermanence.persistedFiles;
     };
   };
 }
