060ce67c2192dac913bc5f0c5e392a2f11d4c2d7
Matthew Rawlings <contact@matthewrawlings.com>
2024-10-05 18:06:00 -0700

    refactor(innistrad): reverse proxy for services

--- machines/servers/innistrad/default.nix
+++ machines/servers/innistrad/default.nix
@@ -8,7 +8,11 @@
   remoteDir,
   localDir,
   ...
-}: {
+}: let
+  jellyfinDomain = "fin.us-drig-1.drigs.org";
+  jellyfinPublic = "fin.drigs.org";
+  jellySeerDomain = "requests.us-drig-1.drigs.org";
+in {
   imports = [
     ../../../modules/system
     ./hardware-configuration.nix
@@ -44,4 +48,55 @@
     intelTranscoding = true;
     jellyseerr = true;
   };
+
+  # reverse proxy
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts = {
+      ${jellyfinDomain} = {
+        useACMEHost = jellyfinDomain;
+        forceSSL = true;
+        locations."/".proxyPass = "http://127.0.0.1:8096";
+        extraConfig = ''
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection $http_connection;
+        '';
+      };
+      ${jellySeerDomain} = {
+        useACMEHost = jellySeerDomain;
+        forceSSL = true;
+        locations."/".proxyPass = "http://127.0.0.1:5055";
+      };
+    };
+  };
+
+  # certs
+  sops.secrets."cloudflare/drigs/token" = {};
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "services@matthewrawlings.com";
+      environmentFile = config.sops.secrets."cloudflare/drigs/token".path;
+      dnsProvider = "cloudflare";
+      dnsResolver = "1.1.1.1:53";
+      dnsPropagationCheck = true;
+      group = config.services.nginx.group;
+    };
+    certs = {
+      ${jellyfinDomain} = {
+        domain = jellyfinDomain;
+        extraDomainNames = [jellyfinPublic];
+      };
+      ${jellySeerDomain} = {
+        domain = jellySeerDomain;
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [80 443];
 }
