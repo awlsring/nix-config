387274bf6d5b69816a0a73afc10deeed6717b895
Matthew Rawlings <contact@matthewrawlings.com>
2025-01-11 22:57:25 -0800

    feat(innistrad): static ip and expanded nameservers

--- machines/servers/innistrad/default.nix
+++ machines/servers/innistrad/default.nix
@@ -27,6 +27,19 @@ in {
     class = "server";
   };
 
+  # TODO: make this part of machine module
+  networking.interfaces.enp89s0.ipv4.addresses = [
+    {
+      address = "10.0.10.120";
+      prefixLength = 24;
+    }
+  ];
+  networking.defaultGateway = {
+    interface = "enp89s0";
+    address = "10.0.10.1";
+  };
+  networking.nameservers = ["1.1.1.1" "10.10.10.10" "100.100.100.100"];
+
   # enable monitoring
   monitoring.node-exporter.enable = true;
 
